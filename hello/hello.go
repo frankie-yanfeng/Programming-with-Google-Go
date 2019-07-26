583762ba862707da77f5a239ac67364cc1137474
frankie-yanfeng <frankie.yanfeng@gmail.com>
2019-07-26 16:06:41 +0800

    first library

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/frankie-yanfeng/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	//fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
