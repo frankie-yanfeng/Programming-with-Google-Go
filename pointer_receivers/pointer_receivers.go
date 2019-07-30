b1cc8ff2eda0030ff5005edabc7809d7ba7f6232
Yan Feng <frankie.yanfeng@gmail.com>
2019-07-30 13:39:07 +0800

    pointer receiver

---
+++ pointer_receivers/pointer_receivers.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//Point definition
+type Point struct {
+	x float64
+	y float64
+}
+
+//OffsetX definition
+func (p *Point) OffsetX(v float64) {
+	fmt.Println(p.x)
+	p.x = p.x + v
+
+	fmt.Println(p.x)
+}
+
+func main() {
+	m := Point{3, 4}
+
+	m.OffsetX(5)
+}
