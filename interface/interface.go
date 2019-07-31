9a88c3441873e3d39a3d8fcf6a34d50d67b5e2e1
Yan Feng <frankie.yanfeng@gmail.com>
2019-07-31 09:58:40 +0800

    try out the interface implementation

--- interface/interface.go
+++ interface/interface.go
@@ -16,8 +16,14 @@ type Dog struct{ name string }
 func (d Dog) Speak() { fmt.Println(d.name) }
 
 func main() {
-	var s1 Speaker
-	d1 := Dog{"Brian"}
-	s1 = d1
-	s1.Speak()
+	// var s1 Speaker
+	// d1 := Dog{"Brian"}
+	// s1 = d1
+	// s1.Speak()
+
+	d := Dog{"Brian"}
+	var s Speaker = d
+	s.Speak()
+
+	d.Speak()
 }
